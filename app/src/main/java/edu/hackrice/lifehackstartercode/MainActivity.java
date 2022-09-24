8a0cf81cc0a6ba5764cc43f917be41075d32e11e
Richard Xu <richard@richards-mbp-4.dyndns.rice.edu>
2022-09-24 15:50:00 -0500

    buttons + text

--- app/src/main/java/edu/hackrice/lifehackstartercode/MainActivity.java
+++ app/src/main/java/edu/hackrice/lifehackstartercode/MainActivity.java
@@ -10,35 +10,18 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonChangeGreeting;
-    Button buttonGoToWelcome;
-    TextView textViewGreeting;
+    Button buttonGoToMain;
+    TextView textViewDayRater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonChangeGreeting = findViewById(R.id.buttonChangeGreeting);
-        buttonGoToWelcome = findViewById(R.id.buttonGoToWelcome);
-        textViewGreeting = findViewById(R.id.textHello);
+        buttonGoToMain = findViewById(R.id.buttonGoToMainActivity);
+        textViewDayRater = findViewById(R.id.textDayRater);
 
-        View.OnClickListener buttonChangeGreetingClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                textViewGreeting.setText("Hello Hackers!");
-            }
-        };
-        buttonChangeGreeting.setOnClickListener(buttonChangeGreetingClickListener);
 
-        View.OnClickListener buttonGoToWelcomeClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, WelcomeActivity.class);
-                i.putExtra("message", "Welcome to HackRice 12 First Timers Track!");
-                startActivity(i);
-            }
-        };
-        buttonGoToWelcome.setOnClickListener(buttonGoToWelcomeClickListener);
+
     }
 }
